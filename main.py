0f5ccc4c3239ef1f79d18f5675b6cbbb77600ee4
lhwxl <lhw100501@163.com>
2024-05-23 21:01:55 +0800

    调试模式

--- main.py
+++ main.py
@@ -2,12 +2,20 @@ import ssd1306py as screen
 from machine import Pin, I2C
 
 
-def init_screen():
+def init_screen():  #13,14引脚
 	pass
 
 
 def main():
 	pass
 
+
 if __name__ == "__main__":
-	main()
+	run_value = Pin(4, Pin.IN, Pin.PULL_UP)
+	if run_value.value():
+		_ = Pin(4, Pin.OUT)
+		_.off()
+		del _
+		del run_value
+
+		main()
